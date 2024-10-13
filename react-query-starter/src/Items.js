5a91553c79e67bf4fc82b24ba831eff8e37cf461
pavanajmera <ajmerapavan20@gmail.com>
2024-10-13 23:26:17 +0530

    rendering data and error handling using useQueryHook

--- react-query-starter/src/Items.js
+++ react-query-starter/src/Items.js
@@ -1,11 +1,39 @@
+import { useQuery } from '@tanstack/react-query';
 import SingleItem from './SingleItem';
+import customFetch from './utils/utils';
 const Items = ({ items }) => {
-  return (
-    <div className='items'>
-      {items.map((item) => {
-        return <SingleItem key={item.id} item={item} />;
-      })}
-    </div>
-  );
+
+    // Method - 1 for fetching data
+    // const result = useQuery({
+    //     queryKey: ['task'],
+    //     queryFn: () => customFetch.get('/')
+    // })
+    // console.log(result)
+
+    // Method - 2 for destructing data
+    const {data, isLoading, isError, error} = useQuery({
+        queryKey: ['task'],
+        queryFn: () => customFetch.get('/1234')
+    })
+
+    if(isLoading){
+        return <p style={{marginTop:'1rem'}}> Loading...</p>
+    }
+
+    if(isError){
+        return <p style={{marginTop:'1rem'}}>There was an error</p>
+    }
+
+    // if(error){
+    //     return <p style={{marginTop:'1rem'}}>{error.response.data}</p>
+    // }
+
+    return (
+        <div className='items'>
+            {data?.data?.taskList.map((item) => {
+                return <SingleItem key={item.id} item={item} />;
+            })}
+        </div>
+    );
 };
 export default Items;
